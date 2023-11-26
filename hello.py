8af4241b7018191b269f15e1e42d33a333ca0dda
Jason Henrique <jhmr.jason@outlook.com>
2023-11-26 10:53:28 -0300

    Insert if

--- hello.py
+++ hello.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python3
+"""Hello World Multi Linguas.
 
-# Este programa e meu primeiro script
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
+correspondente
 
-print("Hello World")
+Como usar:
+Tenha a variavel LANG devidamente configurada exemplo:
+
+    export LANG=en_US.UTF-8
+
+Execução:
+    ./hello.py ou python3 hello.py
+"""
+__version__ = "0.0.1"
+__author__  = "Jason Henrique"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+msg = "Hello World"
+
+if current_language == "pt_BR":
+    msg = "Ola Mundo"
+
+print(msg)
+
+
