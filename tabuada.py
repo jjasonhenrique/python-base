a71bfa0c4bb2b5ae235715726742b1d1599da1d3
Jason Henrique <jhmr.jason@outlook.com>
2023-12-17 09:02:03 -0300

    Update tabuada.py

--- tabuada.py
+++ tabuada.py
@@ -3,14 +3,14 @@
 Imprime a tabuada de 1 a 10
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__  = "Jason"
 
 base = list(range(1,11))
 
-for numero in base:
-    print("A tabuada do numero", numero, ":")
-    for outro_numero in base:
-        resultado = numero * outro_numero
-        print(numero, "X", outro_numero, "=", resultado) 
-    print("-----------------------------")
+for n1 in base:
+    print("{:-^170}".format(f"Tabuada do numero {n1}:")+ "\n")
+    for n2 in base:
+        resultado = n1 * n2
+        print("{:^170}".format(f"{n1} X {n2} = {resultado}"))
+    print("#" * 170 + "\n")
